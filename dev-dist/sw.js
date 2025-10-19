139a3207194102caadcc5c6a9f2627b5f72038e1
freejob985 <freejob985@gmail.com>
2025-10-20 02:41:42 +0300

    Update service worker cache revision for index.html to ensure proper asset management

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0p6v1881rso"
+    "revision": "0.m7iqee8bfso"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
